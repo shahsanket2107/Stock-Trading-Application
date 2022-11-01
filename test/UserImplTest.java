658eab7337437f8afc18f2c8836282eae499f1c8
Sanket Shah <shahsanket2107@gmail.com>
2022-11-01 12:40:53 -0400

    Tests enhanced and jar created

--- test/UserImplTest.java
+++ test/UserImplTest.java
@@ -14,45 +14,39 @@ import static org.junit.Assert.assertEquals;
 
 public class UserImplTest {
   private String name;
-  private ArrayList<Portfolio> list;
-  private Map<String, Integer> m;
-  private Map<String, Integer> m1;
-  private Map<String, Integer> m2;
-  private Portfolio p;
-  private Portfolio p1;
-  private Portfolio p2;
   private User u;
 
   @Before
   public void setup() {
+    Portfolio p;
+    ArrayList<Portfolio> list = new ArrayList<>();
     name = "John Doe";
-    m = new HashMap<>();
-    m1 = new HashMap<>();
-    m2 = new HashMap<>();
-    list = new ArrayList<>();
+    Map<String, Integer> m = new HashMap<>();
+    Map<String, Integer> m1 = new HashMap<>();
+    Map<String, Integer> m2 = new HashMap<>();
     m.put("aapl", 20);
     p = new PortfolioImpl("test_portfolio_single_stock", m);
     list.add(p);
     m1.put("aapl", 20);
     m1.put("googl", 5);
-    p1 = new PortfolioImpl("test_portfolio_two_stock", m1);
-    list.add(p1);
+    p = new PortfolioImpl("test_portfolio_two_stock", m1);
+    list.add(p);
     m2.put("aapl", 20);
     m2.put("googl", 5);
     m2.put("tsla", 22);
-    p2 = new PortfolioImpl("test_portfolio_three_stock", m2);
-    list.add(p2);
+    p = new PortfolioImpl("test_portfolio_three_stock", m2);
+    list.add(p);
     u = new UserImpl(name, list);
   }
 
   @Test
-  public void getName() {
+  public void testGetName() {
     assertEquals("John Doe", u.getName());
   }
 
 
   @Test
-  public void getPortfoliosName() {
+  public void testGetPortfoliosName() {
     assertEquals("The list of portfolios is:\n" +
             "test_portfolio_single_stock\n" +
             "test_portfolio_two_stock\n" +
@@ -60,27 +54,75 @@ public class UserImplTest {
   }
 
   @Test
-  public void createPortfolio() {
+  public void testValidCreatePortfolioForUserWithNoPortfolios() {
+    Map<String, Integer> m = new HashMap<>();
+    User u1 = new UserImpl();
+    m.put("tsla", 10);
+    m.put("zs", 13);
+    m.put("amzn", 3);
+    u1.createPortfolio("testNewPortfolio", m);
+    assertEquals("The list of portfolios is:\n" +
+            "testNewPortfolio\n", u1.getPortfoliosName().toString());
+    assertEquals("Portfolio_Name: testNewPortfolio\n" +
+            "{\n" +
+            "\tStock_Ticker: tsla\n" +
+            "\tQuantity: 10\n" +
+            "}\n" +
+            "{\n" +
+            "\tStock_Ticker: zs\n" +
+            "\tQuantity: 13\n" +
+            "}\n" +
+            "{\n" +
+            "\tStock_Ticker: amzn\n" +
+            "\tQuantity: 3\n" +
+            "}\n" +
+            "\n\n", u1.getPortfolioComposition("testNewPortfolio").toString());
   }
 
   @Test
-  public void loadPortfolio() {
+  public void testValidCreatePortfolioForUserWithExistingPortfolios() {
+    Map<String, Integer> m = new HashMap<>();
+    User u1 = new UserImpl();
+    m.put("tsla", 10);
+    m.put("zs", 13);
+    m.put("amzn", 3);
+    u1.createPortfolio("testNewPortfolio", m);
+    assertEquals("The list of portfolios is:\n" +
+            "testNewPortfolio\n", u1.getPortfoliosName().toString());
+    assertEquals("Portfolio_Name: testNewPortfolio\n" +
+            "{\n" +
+            "\tStock_Ticker: tsla\n" +
+            "\tQuantity: 10\n" +
+            "}\n" +
+            "{\n" +
+            "\tStock_Ticker: zs\n" +
+            "\tQuantity: 13\n" +
+            "}\n" +
+            "{\n" +
+            "\tStock_Ticker: amzn\n" +
+            "\tQuantity: 3\n" +
+            "}\n" +
+            "\n\n", u1.getPortfolioComposition("testNewPortfolio").toString());
   }
 
   @Test
-  public void ifStocksExist() {
+  public void testLoadPortfolio() {
   }
 
   @Test
-  public void isValidFormat() {
+  public void testIfStocksExist() {
   }
 
   @Test
-  public void getTotalValuation() {
+  public void testIsValidFormat() {
   }
 
   @Test
-  public void getPortfolioCompositionForPortfolio3() {
+  public void testGetTotalValuation() {
+  }
+
+  @Test
+  public void testGetPortfolioCompositionForPortfolio3() {
     assertEquals("Portfolio_Name: test_portfolio_three_stock\n" +
             "{\n" +
             "\tStock_Ticker: aapl\n" +
@@ -97,7 +139,7 @@ public class UserImplTest {
   }
 
   @Test
-  public void getPortfolioCompositionForPortfolio2() {
+  public void testGetPortfolioCompositionForPortfolio2() {
     assertEquals("Portfolio_Name: test_portfolio_two_stock\n" +
             "{\n" +
             "\tStock_Ticker: aapl\n" +
@@ -110,7 +152,7 @@ public class UserImplTest {
   }
 
   @Test
-  public void getPortfolioCompositionForPortfolio1() {
+  public void testGetPortfolioCompositionForPortfolio1() {
     assertEquals("Portfolio_Name: test_portfolio_single_stock\n" +
             "{\n" +
             "\tStock_Ticker: aapl\n" +
@@ -119,18 +161,18 @@ public class UserImplTest {
   }
 
   @Test
-  public void getPortfolioCompositionForPortfolioThatDoesNotExist() {
+  public void testGetPortfolioCompositionForPortfolioThatDoesNotExist() {
     assertEquals("The given portfolio name does not exist!!\n" +
             "Please enter a valid portfolio name!!", u.getPortfolioComposition
             ("Not_existing_portfolio").toString());
   }
 
   @Test
-  public void checkPortfolioExists() {
-    assertEquals(true,u.checkPortfolioExists("test_portfolio_single_stock"));
-    assertEquals(true,u.checkPortfolioExists("test_portfolio_two_stock"));
-    assertEquals(true,u.checkPortfolioExists("test_portfolio_three_stock"));
-    assertEquals(false,u.checkPortfolioExists("Not_existing_portfolio"));
+  public void testCheckPortfolioExists() {
+    assertEquals(true, u.checkPortfolioExists("test_portfolio_single_stock"));
+    assertEquals(true, u.checkPortfolioExists("test_portfolio_two_stock"));
+    assertEquals(true, u.checkPortfolioExists("test_portfolio_three_stock"));
+    assertEquals(false, u.checkPortfolioExists("Not_existing_portfolio"));
 
   }
 }
