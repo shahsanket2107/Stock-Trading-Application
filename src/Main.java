ab5a6a19e98915e3d8f36e41cf5f09c7d1330e53
shahsamved <shahsamved@gmail.com>
2022-11-26 13:49:12 -0500

    MR resolve-1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import controller.Controller;
 import controller.UserControllerImpl;
-import java.util.Scanner;
 import model.UserImpl;
-import view.IView;
-import view.JFrameView;
 import view.ViewImpl;
 
 /**
@@ -13,23 +9,6 @@ import view.ViewImpl;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Enter 1 for text-based view and 2 for GUI view: ");
-    Scanner scan = new Scanner(System.in);
-    switch (scan.nextLine()){
-      case "1":
-        new UserControllerImpl(System.in, System.out, new UserImpl(), new ViewImpl()).runGo();
-        break;
-      case "2":
-        System.out.println("Please enter your name: ");
-        String name = scan.nextLine();
-        Controller controller = new Controller(new UserImpl(),name);
-        IView view = new JFrameView(name);
-        controller.setView(view);
-        break;
-      default:
-        System.out.println("Invalid input!");
-    }
-
-
+    new UserControllerImpl(System.in, System.out, new UserImpl(), new ViewImpl()).runGo();
   }
 }
