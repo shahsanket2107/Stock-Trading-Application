452253a500e68f7a57afec51dd2e7f8c3182b553
Sanket <shahsanket2107@gmail.com>
2022-11-28 16:40:28 -0500

    Tests for New Features done

--- test/UserModelExtensionImplTest.java
+++ test/UserModelExtensionImplTest.java
@@ -77,8 +77,85 @@ public class UserModelExtensionImplTest {
             2000, m, 20);
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void investFractionalPercentageWithNegativeAmount() {
+    assertEquals("The list of portfolios is:\n" +
+            "p1\n" +
+            "sanket_p1\n", user.getPortfoliosName().toString());
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.investFractionalPercentage("testPortfolio", "2022-11-19",
+            -1000, m, 20);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void investFractionalPercentageWithNegativeCommissionFees() {
+    assertEquals("The list of portfolios is:\n" +
+            "p1\n" +
+            "sanket_p1\n", user.getPortfoliosName().toString());
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.investFractionalPercentage("testPortfolio", "2022-11-19",
+            1000, m, -20);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void investFractionalPercentageWithFractionalSharesLessThanHundred() {
+    assertEquals("The list of portfolios is:\n" +
+            "p1\n" +
+            "sanket_p1\n", user.getPortfoliosName().toString());
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 5.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.investFractionalPercentage("testPortfolio", "2022-11-19",
+            1000, m, 20);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void investFractionalPercentageWithFractionalSharesGreaterThanHundred() {
+    assertEquals("The list of portfolios is:\n" +
+            "p1\n" +
+            "sanket_p1\n", user.getPortfoliosName().toString());
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 45.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.investFractionalPercentage("testPortfolio", "2022-11-19",
+            1000, m, 20);
+  }
+
   @Test
-  public void testCreateDollarCostAveragePortfolio() {
+  public void testCreateDollarCostAveragePortfolioWithoutFutureEndDate() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "2021-06-19", "2022-11-19", 30);
+    assertEquals(true, test);
+    assertEquals("The portfolio composition of portfolio test_p1 on 2022-11-19 is:-\n" +
+                    "{\n" +
+                    "  \"MSFT\" : 12.792661709978818,\n" +
+                    "  \"AAPL\" : 129.13769875997562,\n" +
+                    "  \"TSLA\" : 6.287988662332346,\n" +
+                    "  \"ZS\" : 43.90180649735259\n" +
+                    "}",
+            user.getFlexiblePortfolioComposition("test_p1", "2022-11-19")
+                    .toString().replace("\r", ""));
+  }
+
+  @Test
+  public void testCreateDollarCostAveragePortfolioWithFutureEndDate() {
     Map<String, Double> m = new HashMap();
     m.put("AAPL", 55.0);
     m.put("ZS", 25.0);
@@ -87,7 +164,15 @@ public class UserModelExtensionImplTest {
     boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
             20, "2021-06-19", "2023-11-19", 30);
     assertEquals(true, test);
-    //assertEquals("", user.getFlexiblePortfolioComposition("test_p1", "2022-11-19"));
+    assertEquals("The portfolio composition of portfolio test_p1 on 2022-11-19 is:-\n" +
+                    "{\n" +
+                    "  \"MSFT\" : 12.792661709978818,\n" +
+                    "  \"AAPL\" : 129.13769875997562,\n" +
+                    "  \"TSLA\" : 6.287988662332346,\n" +
+                    "  \"ZS\" : 43.90180649735259\n" +
+                    "}",
+            user.getFlexiblePortfolioComposition("test_p1", "2022-11-19")
+                    .toString().replace("\r", ""));
   }
 
   @Test
@@ -100,11 +185,18 @@ public class UserModelExtensionImplTest {
     boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
             20, "2021-06-19", "", 30);
     assertEquals(true, test);
-    //assertEquals("", user.getFlexiblePortfolioComposition("test_p1", "2022-11-19"));
+    assertEquals("The portfolio composition of portfolio test_p1 on 2022-11-19 is:-\n" +
+            "{\n" +
+            "  \"MSFT\" : 12.792661709978818,\n" +
+            "  \"AAPL\" : 129.13769875997562,\n" +
+            "  \"TSLA\" : 6.287988662332346,\n" +
+            "  \"ZS\" : 43.90180649735259\n" +
+            "}", user.getFlexiblePortfolioComposition("test_p1", "2022-11-19")
+            .toString().replace("\r", ""));
   }
 
   @Test
-  public void testExistingDollarCostAveragePortfolio() {
+  public void testExistingDollarCostAveragePortfolioWithoutFutureDate() {
     Map<String, Double> m = new HashMap();
     m.put("AAPL", 55.0);
     m.put("ZS", 25.0);
@@ -112,7 +204,126 @@ public class UserModelExtensionImplTest {
     boolean test = user.dollarCostAveragingPortfolio("sanket_p1", m, 2000,
             20, "2022-06-19", "2022-11-19", 30);
     assertEquals(true, test);
-    assertEquals("", user.getFlexiblePortfolioComposition("sanket_p1", "2022-11-19"));
+    assertEquals("The portfolio composition of portfolio sanket_p1 on 2022-11-19 is:-\n" +
+            "{\n" +
+            "  \"AAPL\" : 48.495596260544055,\n" +
+            "  \"TSLA\" : 16.735469413477954,\n" +
+            "  \"ZS\" : 34.0640629821031\n" +
+            "}", user.getFlexiblePortfolioComposition("sanket_p1", "2022-11-19")
+            .toString().replace("\r", ""));
+  }
+
+  @Test
+  public void testExistingDollarCostAveragePortfolioWithFutureDate() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 20.0);
+    boolean test = user.dollarCostAveragingPortfolio("sanket_p1", m, 2000,
+            20, "2022-06-19", "2023-11-19", 30);
+    assertEquals(true, test);
+    assertEquals("The portfolio composition of portfolio sanket_p1 on 2022-11-19 is:-\n" +
+            "{\n" +
+            "  \"AAPL\" : 48.495596260544055,\n" +
+            "  \"TSLA\" : 16.735469413477954,\n" +
+            "  \"ZS\" : 34.0640629821031\n" +
+            "}", user.getFlexiblePortfolioComposition("sanket_p1", "2022-11-19")
+            .toString().replace("\r", ""));
+  }
+
+  @Test
+  public void testExistingDollarCostAveragePortfolioWithNoEndDate() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 20.0);
+    boolean test = user.dollarCostAveragingPortfolio("sanket_p1", m, 2000,
+            20, "2022-06-19", "", 30);
+    assertEquals(true, test);
+    assertEquals("The portfolio composition of portfolio sanket_p1 on 2022-11-19 is:-\n" +
+            "{\n" +
+            "  \"AAPL\" : 48.495596260544055,\n" +
+            "  \"TSLA\" : 16.735469413477954,\n" +
+            "  \"ZS\" : 34.0640629821031\n" +
+            "}", user.getFlexiblePortfolioComposition("sanket_p1", "2022-11-19")
+            .toString().replace("\r", ""));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithNegativeAmount() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, -2000,
+            20, "2021-06-19", "2022-11-19", 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithNegativeCommissionFees() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            -20, "2021-06-19", "2022-11-19", 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithNegativeInterval() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "2021-06-19", "2022-11-19", -3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithUnformattedStartDate() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "19-06-2022", "2022-11-19", 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithUnformattedEndDate() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 25.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "2022-06-19", "19-11-2022", 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithFractionalShareSumLessThanHundred() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 5.0);
+    m.put("TSLA", 10.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "2021-06-19", "2022-11-19", 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateDollarCostAveragePortfolioWithFractionalShareSumMoreThanHundred() {
+    Map<String, Double> m = new HashMap();
+    m.put("AAPL", 55.0);
+    m.put("ZS", 35.0);
+    m.put("TSLA", 20.0);
+    m.put("MSFT", 10.0);
+    boolean test = user.dollarCostAveragingPortfolio("test_p1", m, 2000,
+            20, "2021-06-19", "2022-11-19", 30);
   }
 
 
